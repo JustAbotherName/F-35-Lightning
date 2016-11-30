dd54243708f845cd416824482f5c2e2cdeff05e9
GabyTheCoder <gabriel.selegean@yahoo.ro>
2016-11-30 20:49:25 +0200

    added help button

--- src/ui/menu.cpp
+++ src/ui/menu.cpp
@@ -7,10 +7,16 @@ SimpleMenu::SimpleMenu(QWidget *parent)
     : QMainWindow(parent) {
     
   QAction *quit = new QAction("&Quit", this);
+  QAction *help = new QAction("&Help", this);
+
 
   QMenu *file;
   file = menuBar()->addMenu("&File");
   file->addAction(quit);
 
+  file = menuBar()->addMenu("&Help");
+  file->addAction(help);
+
   connect(quit, &QAction::triggered, qApp, QApplication::quit);
+  connect(help, &QAction::triggered, qApp, QApplication::quit);
 }
